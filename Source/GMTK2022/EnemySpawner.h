e3993dd275eff087914b90d064f38b3ced3b0a4b
JackP-Dev <38892053+JackP-Dev@users.noreply.github.com>
2022-07-16 13:48:20 +0100

    Basic Enemy spawner added

---
+++ Source/GMTK2022/EnemySpawner.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "EnemySpawner.generated.h"
+
+UCLASS()
+class GMTK2022_API AEnemySpawner : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AEnemySpawner();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+private:
+	void SpawnEnemy();
+private:
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<AActor> EnemyToSpawn;
+
+	int EnemyCount = 0;
+	
+	UPROPERTY(EditAnywhere)
+	int MaxEnemyCount = 10;
+
+};
---
+++ enc_temp_folder/e9ba93104acd56bca1ed31629074917d/EnemySpawner.cpp
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EnemySpawner.h"
+
+// Sets default values
+AEnemySpawner::AEnemySpawner()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AEnemySpawner::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	SpawnEnemy();
+}
+
+// Called every frame
+void AEnemySpawner::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (EnemyCount != MaxEnemyCount)
+	{
+		SpawnEnemy();
+		
+	}
+}
+
+void AEnemySpawner::SpawnEnemy()
+{
+	if (EnemyToSpawn)
+	{
+		GetWorld()->SpawnActor<AActor>(EnemyToSpawn, GetActorLocation(), GetActorRotation());
+		++EnemyCount;
+	}
+}
+
